39e8f28e061a76ace959111049c7da1c162a025e
Simone Basso <bassosimone@gmail.com>
2025-11-12 15:34:53 +0100

    feat: add initial, static data
    This diff adds initial, static data to the repository.
    
    Along with the static data, we include the queries and the
    scripts used to generate the static data.

---
+++ data/query_uploads.sql
@@ -0,0 +1,20 @@
+SELECT
+    client.Geo.CountryCode as country_code,
+    COUNT(*) as sample_count,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(1)] as upload_p1,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(5)] as upload_p5,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(10)] as upload_p10,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(25)] as upload_p25,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(50)] as upload_p50,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(75)] as upload_p75,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(90)] as upload_p90,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(95)] as upload_p95,
+    APPROX_QUANTILES(a.MeanThroughputMbps, 100)[OFFSET(99)] as upload_p99
+FROM
+    `measurement-lab.ndt.unified_uploads`
+WHERE
+    date BETWEEN "2024-10-01" AND "2024-10-31"
+    AND client.Geo.CountryCode IN ("US", "DE", "BR")
+    AND a.MeanThroughputMbps IS NOT NULL
+GROUP BY country_code
+ORDER BY country_code

c4065eb9d7fee21ed35895fc3fbc3e864024a26b
Simone Basso <bassosimone@gmail.com>
2025-12-24 14:34:26 +0100

    feat(scripting): add exception interceptor
    This functionality allows to intercept and log exceptions
    without too many try..catch and unwinding.
    
    It also allow to get the proper sys.exit exitcode.
    
    I tested this functionality briefly in an integration
    branch, and it seems to help to have better UX while
    at the same time having a reasonable DX.

---
+++ library/src/iqb/scripting/iqb_exception.py
@@ -0,0 +1,46 @@
+"""Optional scripting helpers to log exceptions and convert them to exit codes."""
+
+from __future__ import annotations
+
+from .iqb_logging import log
+
+
+class Interceptor:
+    """
+    Context manager to intercept exceptions.
+
+    Use as a context manager:
+
+        interceptor = iqb_exception.Interceptor()
+        with interceptor:
+            func()
+        print(interceptor.failed)
+        sys.exit(interceptor.exitcode())
+
+    Exceptions are logged and suppressed. The failed field
+    tells you whether there were any exceptions.
+
+    Use interceptor.exitcode() to get the suitable exit code
+    to pass to the sys.exit() function.
+    """
+
+    def __init__(self):
+        self.failed = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            log.error("operation failed: %s", exc_value)
+            _ = traceback
+            self.failed = True
+        return True  # suppress the exception
+
+    def exitcode(self) -> int:
+        """
+        Return the exitcode to pass to sys.exit.
+
+        Zero on success, 1 on failure.
+        """
+        return int(self.failed)
